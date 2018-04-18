2be99f6d9dcfbd8c6a51196a577fa293fb1db7fd
Martin Průcha <martin.prucha@factorio.cz>
2018-04-18 18:01:43 +0200

    remove method merchant_pem

--- lib/gp_webpay/config.rb
+++ lib/gp_webpay/config.rb
@@ -18,7 +18,7 @@ module GpWebpay
   class Configuration #:nodoc:
     include ActiveSupport::Configurable
     config_accessor :merchant_number
-    config_accessor :merchant_pem_path
+    config_accessor :merchant_pem
     config_accessor :merchant_password
     config_accessor :gpe_pem_path
     config_accessor :environment
@@ -45,14 +45,6 @@ module GpWebpay
       end
     end
 
-    def merchant_pem
-      if config.merchant_pem_path
-        File.read config.merchant_pem_path
-      else
-        nil
-      end
-    end
-
     def gpe_pem
       if config.gpe_pem_path
         File.read config.gpe_pem_path
@@ -74,7 +66,7 @@ module GpWebpay
   # this is ugly. why can't we pass the default value to config_accessor...?
   configure do |config|
     config.merchant_number    = nil
-    config.merchant_pem_path  = nil
+    config.merchant_pem       = nil
     config.merchant_password  = nil
     config.gpe_pem_path       = nil
     config.environment        = defined?(Rails) && Rails.env || 'test'
